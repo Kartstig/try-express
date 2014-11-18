4425515a6eb2c3edb128c06f13bbc00a00ea1954
Herman Singh <kartstig@gmail.com>
2014-11-18 12:22:25 -0500

    add mysql support

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var connection = require('express-myconnection');
+var mysql = require('mysql');
+
+app.use(
+        connection(mysql, {
+            host: 'localhost',
+            user: 'root',
+            password: 'test123',
+            port: 3306,
+            database: 'test_db'
+        }, 'request')
+);
 
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/templates/index.html');
