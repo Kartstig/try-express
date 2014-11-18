1d771344b4c90bc501a47d6bac1ce22bba1dbf1f
Herman Singh <kartstig@gmail.com>
2014-11-18 00:18:02 -0500

    Add user joined

--- index.js
+++ index.js
@@ -3,15 +3,18 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/templates/index.html');
+  res.sendFile(__dirname + '/templates/index.html');
 });
 
 io.on('connection', function(socket){
+  socket.emit('chat message', function () {
+    io.emit('chat message', 'A user has joined')
+  });
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
   });
 });
 
 http.listen(4000, function () {
-    console.log('listening on *:4000');
+  console.log('listening on *:4000');
 });
